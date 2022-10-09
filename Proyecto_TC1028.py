1c108ab4fd79598636513340d963e9e846e2ece3
Jose Eduardo <a01710605@tec.mx>
2022-10-09 15:32:49 -0500

    Dividi todo el codigo aparte , en pequeñas funciones

--- Proyecto_TC1028.py
+++ Proyecto_TC1028.py
@@ -1,112 +1,131 @@
-"""
-Proyecto TC1028
-"""
+("####### PROYECTO TC1028 ########")
 import os
 
+#================== Variables Globales  =======================================
+global lista
+lista=[]
 
-print("#################### ANALIZADoR DE CARRERAS ########################")
+#================== funciones auxiliares  =======================================
 
-entrada1 = input(f"pregunta 1: De las siguientes materias ¿Cual es tu favorita?: \n\n\t\t1. Matematicas-Fisica\n\t\t2. Computacion\n\t\t3. Quimica\n\nRespuesta: ")
+def valor_preguntas (entrada):
 
-While = (entrada1 == "1" or "uno"):
-respuesta1 = 10
-if (entrada1 == "2" or "dos"): 
-    respuesta1 = 20
-elif (entrada1 == "3" or "tres"):
-    respuesta1 = 30 
+ if entrada == "1":
+    res = 10
+ elif entrada == "2":
+    res = 20
+ elif entrada == "3":
+    res = 30
 
-print(f"El valor de respuesta1 es: {respuesta1}")
-    
-"""""
-entrada2 = input(f"pregunta 2: Para ti ¿Que es la ingenieria?: \n\n\t1. Es el poder crear cosas atarves de la indrustria\n\n\t2. Es el saber como funcionan los aparatos electronicos por dentro\n\n\t3. Es el poder crear soluciones a porblemas atraves de un laboratorio \n\nRespuesta: ")
+ return res 
+   
+def contador_respuestas(respuestas):
+   global lista
+   lista.append(respuestas)       
+   print("Tu resultado es: \t", lista)
+   return lista
+
+def total ():
+   global lista
+   total=0
+   for i in lista:
+      total=total+i
+   
+   print("\t (⌐■_■) El total de puntos es: \t", total)
+   return total
+
+#==================Funcion Auxiliar==================
+
+def pausa ():
+    reset = input("Presiona cualquier boton para pasar a la siguiente pregunta (☞ﾟヮﾟ)☞")
+    os.system("cls")
+
+#==================Parte principal del programa==================
+
+print("BIENVENIDO AL")
+print("\n\t\t\t(*￣3￣) ANALIZADOR DE CARRERAS\t( ͡° ͜ʖ ͡°)")
+print("\n\n(★‿★) DONDE TE ORIENTAREMOS A DECIDIR CUAL ES LA MEJOR CARRERA PARA TI POR FAVOR CONTESTA LAS SIGUIENTES PREGUNTAS (●'◡'●)\n\n")
+
+
+pregunta_1 = input("Pregunta 1: \nDe las siguientes materias ¿Cual es tu favorita?: \n\n\t\t1. Matematicas-Fisica\n\t\t2. Computacion\n\t\t3. Quimica\n\nRespuesta: ")
 os.system("cls")
-entrada3 = input(f"pregunta 3: ¿Cual crees que sea tu mejor fortaleza?: \n\n\t1. Analisis\n\n\t2. Control bajo estres\n\n\t3. Trabajo en equipo\n\nRespuesta: " )
+while pregunta_1 >= "4":
+   print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
+   pregunta_1=input("Pregunta 1: \nDe las siguientes materias ¿Cual es tu favorita?: \n\n\t\t1. Matematicas-Fisica\n\t\t2. Computacion\n\t\t3. Quimica\n\nRespuesta: ")
+   os.system("cls")
+
+contador_respuestas(valor_preguntas(pregunta_1))
+pausa()
+
+pregunta_2 = input("Pregunta 2: \nPara ti ¿Que es la ingenieria?: \n\n\t1. Es el poder crear cosas atarves de la indrustria\n\n\t2. Es el saber como funcionan los aparatos electronicos por dentro\n\n\t3. Es el poder crear soluciones a porblemas atraves de un laboratorio \n\nRespuesta: ")
 os.system("cls")
-entrada4 = input("pregunta 4: ¿En que area Ingenieril te gustaria desempeñarte?: \n\n\t1. Mecatronica y Robotica\n\n\t2. Tecnologias Computacionales\n\n\t3. Bioingenierias\n\n\t4. Ciencia Aplicadas\n\nRespuesta: ")
+while pregunta_2 >= "4":
+   print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
+   pregunta_2 = input("Pregunta 2: \nPara ti ¿Que es la ingenieria?: \n\n\t1. Es el poder crear cosas atarves de la indrustria\n\n\t2. Es el saber como funcionan los aparatos electronicos por dentro\n\n\t3. Es el poder crear soluciones a porblemas atraves de un laboratorio \n\nRespuesta: ")
+   os.system("cls")
+
+contador_respuestas(valor_preguntas(pregunta_2))
+pausa()
+
+pregunta_3 = input(f"pregunta 3: ¿Cual crees que sea tu mejor fortaleza?: \n\n\t1. Analisis\n\n\t2. Control bajo estres\n\n\t3. Trabajo en equipo\n\nRespuesta: " )
 os.system("cls")
-entrada5 = input(f"pregunta 5: Del 1 al 3 ¿Como es tu desempeño en las materias fisico-matematicas?: \n\n\t- 1\n\n\t- 2\n\n\t- 3 \n\nRespuesta: ")
+while pregunta_3 >= "4":
+   print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
+   pregunta_3 = input(f"pregunta 3: ¿Cual crees que sea tu mejor fortaleza?: \n\n\t1. Analisis\n\n\t2. Control bajo estres\n\n\t3. Trabajo en equipo\n\nRespuesta: " )
+   os.system("cls")
+
+contador_respuestas(valor_preguntas(pregunta_3))
+pausa()
+
+pregunta_4 = input("pregunta 4: ¿En que area Ingenieril te gustaria desempeñarte?: \n\n\t1. Mecatronica y Robotica\n\n\t2. Tecnologias Computacionales\n\n\t3. Bioingenierias\n\nRespuesta: ")
 os.system("cls")
-entrada6 = input(f"pregunta 6: ¿En que tipo de industria te ves trabajando en un futuro?: \n\n\t1. Manofuctura y diseño mecanico\n\n\t2. Big TECH\n\n\t3. Aeroespacial\n\n\t4. Alimentaria o relacionados\n\n\t5. Centros de Investigacion\n\n\t6. Aeroespacial o Aeronautica \n\nRespuesta: ")
+while pregunta_4 >= "4":
+   print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
+   pregunta_4 = input("pregunta 4: ¿En que area Ingenieril te gustaria desempeñarte?: \n\n\t1. Mecatronica y Robotica\n\n\t2. Tecnologias Computacionales\n\n\t3. Bioingenierias\n\nRespuesta: ")
+   os.system("cls")
+
+contador_respuestas(valor_preguntas(pregunta_4))
+pausa()
+
+pregunta_5 = input("pregunta 5: Del 1 al 3 ¿Como es tu desempeño en las materias fisico-matematicas?: \n\n\t- 1\n\n\t- 2\n\n\t- 3 \n\nRespuesta: ") 
 os.system("cls")
-entrada7 = input(f"pregunta 7: ¿Con cual de las siguientes palabras te identificas: analitico, creativo o ingenioso?: \n\n\t1. Analitico\n\n\t2. Creativo\n\n\t3. Ingenioso\n\nRespuesta: ")
+while pregunta_5 >= "4":
+   print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
+   pregunta_5 = input("pregunta 5: Del 1 al 3 ¿Como es tu desempeño en las materias fisico-matematicas?: \n\n\t- 1\n\n\t- 2\n\n\t- 3 \n\nRespuesta: ")
+   os.system("cls")
+
+contador_respuestas(valor_preguntas(pregunta_5))
+pausa()
+
+pregunta_6 = input("pregunta 6: ¿En que tipo de industria te ves trabajando en un futuro?: \n\n\t1. Manufactura y diseño mecanico\n\n\t2. Big TECH\n\n\t3. Aeroespacial\n\nRespuesta: ")
 os.system("cls")
-entrada8 = input(f"pregunta 8: ¿Eres de un aprendizaje mas visual, emocional o asociativo?: \n\n\t1. Visual\n\n\t2. Emocional\n\n\t2. Asociativo\n\nRespuesta: ")
+while pregunta_6 >= "4":
+   print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
+   pregunta_6 = input("pregunta 6: ¿En que tipo de industria te ves trabajando en un futuro?: \n\n\t1. Manofuctura y diseño mecanico\n\n\t2. Big TECH\n\n\t3. Aeroespacial\n\nRespuesta: ")
+   os.system("cls")
+
+contador_respuestas(valor_preguntas(pregunta_6))
+pausa()
+
+pregunta_7 = input("pregunta 7: ¿Con cual de las siguientes palabras te identificas: analitico, creativo o ingenioso?: \n\n\t1. Analitico\n\n\t2. Creativo\n\n\t3. Ingenioso\n\nRespuesta: ")
 os.system("cls")
+while pregunta_7 >= "4":
+   print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
+   pregunta_7 = input("pregunta 7: ¿Con cual de las siguientes palabras te identificas: analitico, creativo o ingenioso?: \n\n\t1. Analitico\n\n\t2. Creativo\n\n\t3. Ingenioso\n\nRespuesta: ")
+   os.system("cls")
 
-#def pregunta_totales(entrada1,entrada2,entrada3,entrada4,entrada5,entrada6,entrada7,entrada8):
-    
+contador_respuestas(valor_preguntas(pregunta_7))
+pausa()
 
-            
-if(entrada2 == "1" or "uno"):
-    respuesta2 = 10
-elif(entrada2 == "2" or "dos"): 
-    respuesta2 = 20
-elif(entrada2 == "3" or "tres"):
-    respuesta2 = 30
-
-
-if(entrada3 == "1" or "uno"): 
-    respuesta3 = 10
-elif (entrada3 == "2" or "dos"): 
-    respuesta3 = 20
-elif (entrada3 == "3"or "tres"):
-    respuesta3 = 30
-    
-if(entrada4 == "1" or "uno"):
-    respuesta4 = 10
-elif (entrada4 == "2" or "dos"): 
-    respuesta4 = 20
-elif (entrada4 == "3" or "tres"):
-    respuesta4 = 30
-elif (entrada4 == "4" or "cuatro"):
-    respuesta4 = 40
-elif(entrada4 == "5" or "cinco"):
-    respuesta4 = 50
-
-if(entrada5 == "1" or "uno"):
-    respuesta5 = 10
-elif(entrada5 == "2" or "dos"): 
-    respuesta5 = 20
-elif(entrada5 == "3" or "tres"):
-    respuesta5 = 30
-
-if(entrada6 == "1" or "uno"):
-    respuesta6 = 10
-elif (entrada6 == "2" or "dos"): 
-    respuesta6 = 20
-elif (entrada6 == "3" or "tres"):
-    respuesta6 == 30
-elif (entrada6 == "4" or "cuatro"):
-    respuesta6 = 40
-elif(entrada6 == "5" or "cinco"):
-    respuesta6 = 50
-
-if(entrada7 == "1" or "uno"): 
-    respuesta7 = 10
-elif (entrada7== "2" or "dos"): 
-    respuesta7 = 20
-elif (entrada7 == "3" or "tres"):
-    respuesta7 = 30
-
-if(entrada8 == "1" or "uno"): 
-    respuesta8 = 10
-elif (entrada8 == "2" or "dos"): 
-    respuesta8 = 20
-elif (entrada8 == "3" or "tres"):
-    respuesta8 = 30
-    
-puntaje_total = int(respuesta1+respuesta2+respuesta3+respuesta4+respuesta5+respuesta6+respuesta7+respuesta8)
-
-
-"""""
-""" 
- return int (puntaje_total)
+pregunta_8 = input(f"pregunta 8: ¿Eres de un aprendizaje mas visual, emocional o asociativo?: \n\n\t1. Visual\n\n\t2. Emocional\n\n\t3. Asociativo\n\nRespuesta: ")
+os.system("cls")
+while pregunta_8 >= "4":
+   print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
+   pregunta_8 = input(f"pregunta 8: ¿Eres de un aprendizaje mas visual, emocional o asociativo?: \n\n\t1. Visual\n\n\t2. Emocional\n\n\t3. Asociativo\n\nRespuesta: ")
+   os.system("cls")
 
 
 
+contador_respuestas(valor_preguntas(pregunta_8))
+pausa()
 
-rint("######## Resultado del analizador #########")
-print(pregunta_totales(entrada1,entrada2,entrada3,entrada4,entrada5,entrada6,entrada7,entrada8))
-
-"""
-
+total()
