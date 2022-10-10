6fd72d5fb3de1b3af32ad7984dde9f3a93902430
Jose Eduardo <a01710605@tec.mx>
2022-10-10 11:15:53 -0500

    Comente mi codigo

--- Proyecto_TC1028.py
+++ Proyecto_TC1028.py
@@ -1,12 +1,22 @@
 ("####### PROYECTO TC1028 ########")
 import os
 
-#================== Variables Globales  =======================================
+#================== Variables Globales  ===============================
+"""
+Defino una lista global para poder tener libertidad a la hora de ocupar-
+la en varias funciones
+"""
 global lista
 lista=[]
-
-#================== funciones auxiliares  =======================================
-
+global lista_pregntas
+lista_preguntas=[]
+#================== funciones auxiliares  =============================
+"""
+Defini varias funciones que estaria ocupando a lo largo de todo mi codi-
+go, al ser un test con varias preguntas el proceso de estas se repetiria, 
+de esta manera siento que es mejor hacer funciones con instrucciones mas 
+generales para que la funcion aplique co distintos parametros
+"""
 def valor_preguntas (entrada):
 
  if entrada == "1":
@@ -17,21 +27,38 @@ def valor_preguntas (entrada):
     res = 30
 
  return res 
-   
+
+"""
+La funcion valor_preguntas le asigna un valor a cada respuesta dependien-
+do que string reciba
+"""
+
 def contador_respuestas(respuestas):
    global lista
    lista.append(respuestas)       
    print("Tu resultado es: \t", lista)
    return lista
-
+"""
+La funcion contador_respuestas se encarga de meter los valores de las res-
+puestas, adjunta todos esos valores en un lista global (esta la ocupe para
+tener una misma  variable que pueda ocupar en diferentes funciones), a es-
+ta solo le agregue .append para agregar los valores de la funcion pasada, 
+aparte de que esta te va mostrando el valor de la respuesta que hayas metido.
+"""
 def total ():
    global lista
    total=0
    for i in lista:
       total=total+i
-   
+    
    print("\t (⌐■_■) El total de puntos es: \t", total)
    return total
+
+"""
+La funcion total se encarga de sumar los valores de la lista global, suma 
+estos elemtos atraves de un ciclo for y un acumulador, para que al final
+te indique cual es tu resultado final.
+"""
 
 #==================Funcion Auxiliar==================
 
@@ -39,10 +66,15 @@ def pausa ():
     reset = input("Presiona cualquier boton para pasar a la siguiente pregunta (☞ﾟヮﾟ)☞")
     os.system("cls")
 
-#==================Parte principal del programa==================
+"""
+La funcion pausa solo es un pequeño intermedio entre pregunta y pregunta
+"""
 
-print("BIENVENIDO AL")
-print("\n\t\t\t(*￣3￣) ANALIZADOR DE CARRERAS\t( ͡° ͜ʖ ͡°)")
+#==================Cuerpo del programa==================
+
+print("\n\t\t\t\t\tBIENVENIDO AL")
+Nombre_del_proyecto = ("\n\t\t\t(*￣3￣) analizador de ingenierias\t( ͡° ͜ʖ ͡°)")
+print(Nombre_del_proyecto.upper())
 print("\n\n(★‿★) DONDE TE ORIENTAREMOS A DECIDIR CUAL ES LA MEJOR CARRERA PARA TI POR FAVOR CONTESTA LAS SIGUIENTES PREGUNTAS (●'◡'●)\n\n")
 
 
@@ -54,8 +86,17 @@ while pregunta_1 >= "4":
    os.system("cls")
 
 contador_respuestas(valor_preguntas(pregunta_1))
-pausa()
 
+pausa()
+"""
+En esta parte el While crea un ciclo que te regresa a la misma pregunta si 
+es que no se cumple la condicion.
+Abajo del while se ve como invoco las funciones anteriormente definidas para
+poder ir acumulando los valores de las respuestas en la lista global y abajo
+de esa la funcion pausa para dar el intermedio entre pregunta y pregunta
+(Este mismo proceso se repetira con las 13 preguntas que conforman al test)
+
+"""
 pregunta_2 = input("Pregunta 2: \nPara ti ¿Que es la ingenieria?: \n\n\t1. Es el poder crear cosas atarves de la indrustria\n\n\t2. Es el saber como funcionan los aparatos electronicos por dentro\n\n\t3. Es el poder crear soluciones a porblemas atraves de un laboratorio \n\nRespuesta: ")
 os.system("cls")
 while pregunta_2 >= "4":
@@ -116,16 +157,68 @@ while pregunta_7 >= "4":
 contador_respuestas(valor_preguntas(pregunta_7))
 pausa()
 
-pregunta_8 = input(f"pregunta 8: ¿Eres de un aprendizaje mas visual, emocional o asociativo?: \n\n\t1. Visual\n\n\t2. Emocional\n\n\t3. Asociativo\n\nRespuesta: ")
+pregunta_8 = input("pregunta 8: ¿Eres de un aprendizaje mas visual, emocional o asociativo?: \n\n\t1. Visual\n\n\t2. Emocional\n\n\t3. Asociativo\n\nRespuesta: ")
 os.system("cls")
 while pregunta_8 >= "4":
    print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
-   pregunta_8 = input(f"pregunta 8: ¿Eres de un aprendizaje mas visual, emocional o asociativo?: \n\n\t1. Visual\n\n\t2. Emocional\n\n\t3. Asociativo\n\nRespuesta: ")
+   pregunta_8 = input("pregunta 8: ¿Eres de un aprendizaje mas visual, emocional o asociativo?: \n\n\t1. Visual\n\n\t2. Emocional\n\n\t3. Asociativo\n\nRespuesta: ")
    os.system("cls")
 
-
-
 contador_respuestas(valor_preguntas(pregunta_8))
+pausa() 
+
+pregunta_9 = input("pregunta 9: ¿Sientes interes por la electronica y los componentes de las maquinas?: \n\n\t1. Es algo que me encanta \n\n\t2. Me llama la atencion \n\n\t3. No me interesa \n\nRespuesta: ")
+os.system("cls")
+while pregunta_9 >= "4":
+    print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
+    os.system("cls")
+
+contador_respuestas(valor_preguntas(pregunta_9))
+pausa()
+
+pregunta_10 = input("pregunta 10: ¿Trabajando en calidad de alimentos?: \n\n\t1. Me aburriria \n\n\t2. Me llama la atencion \n\n\t3. Me encantaria \n\nRespuesta: ")
+os.system("cls")
+while pregunta_10 >= "4":
+    print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
+    os.system("cls")
+
+contador_respuestas(valor_preguntas(pregunta_10))
+pausa()
+
+pregunta_11 = input("pregunta 11: ¿Disfrutas hacer experimentos con distintas sustancias?: \n\n\t1. Poco\n\n\t2. Mas o memnos \n\n\t3. Bastante\n\nRespuesta: ")
+os.system("cls")
+while pregunta_11 >= "4":
+    print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
+    os.system("cls")
+
+contador_respuestas(valor_preguntas(pregunta_11))
+pausa()
+
+pregunta_12 = input("pregunta 12: ¿Te sientes capacitado para contribuir a un mejor rendimiento en la empresa?: \n\n\t1. Poco\n\n\t2. Mucho\n\n\t3. Bastante\n\nRespuesta: ")
+os.system("cls")
+while pregunta_12 >= "4":
+    print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
+    os.system("cls")
+
+contador_respuestas(valor_preguntas(pregunta_12))
+pausa()
+
+pregunta_13 = input("pregunta 13: ¿Te interesa aprender como una maquina interactua con un humano?: \n\n\t1. No me veo trabajando con tecnologia \n\n\t2. Me llama la atencion, pero prefiero estar en otra rama de la industria\n\n\t3. Me gustaria dedicarme a eso \n\nRespuesta: ")
+os.system("cls")
+while pregunta_13 >= "4":
+    print("\t\t\tRespuesta incorrecta intenta de nuevo  \n\t\t\t\t.·´¯`(>▂<)´¯`·.\n")
+    os.system("cls")
+
+contador_respuestas(valor_preguntas(pregunta_13))
 pausa()
 
 total()
+
+
+lista_preguntas=["pregunta1", "pregunta2", "pregunta_3", "pregunta_4", "pregunta_5", "pregunta_6", "pregunta_7", "pregunta_8"," pregunta_9", "pregunta_10", "pregunta_11", "pregunta_12", "pregunta_13"]
+
+def acum (lista1,lista2):
+    lista=[[lista1],[lista2]]
+    return lista
+
+acum (contador_respuestas(valor_preguntas(pregunta_1)), lista_preguntas )
